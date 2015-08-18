b283646e05354fafc8009ff9b2d2a5db61471dd9
Michael O'Brien <michael_obrien_a@hotmail.com>
2015-08-18 01:40:03 +0100

    changed exports.name and exports.namespace

--- system.js
+++ system.js
@@ -1,8 +1,8 @@
 // Define the system topology here. The topolgy should reference
 // containers defined in definitions/services.js
 
-exports.name = 'ZasBB';
-exports.namespace = 'ZasBB';
+exports.name = 'zasbb';
+exports.namespace = 'zasbb';
 exports.id = 'c83ad36a-185d-4845-b6ca-11ef185ac009';
 
 exports.topology = {
